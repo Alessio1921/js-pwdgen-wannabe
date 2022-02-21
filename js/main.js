61c02df9598659cdc30167391c271084e5492aa3
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-02-21 15:04:45 +0100

    ended the exercise

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const firstName = prompt("inserire il vostro nome");
+const surName = prompt ("inserire il vostro cognome");
+const favoriteColor = prompt("inserire il vostro colore preferito");
+
+document.getElementById("password").innerHTML=firstName + surName + favoriteColor +21;
