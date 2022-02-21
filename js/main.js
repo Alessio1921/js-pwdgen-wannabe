ca167bb2111a79af1d134749d8330f2bf8b11214
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-02-21 15:51:30 +0100

    added aesthetic elements

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ const firstName = prompt("inserire il vostro nome");
 const surName = prompt ("inserire il vostro cognome");
 const favoriteColor = prompt("inserire il vostro colore preferito");
 
-document.getElementById("password").innerHTML=firstName + surName + favoriteColor +21;
+document.getElementById("password").innerHTML=firstName + surName + favoriteColor + 21;
